a36af7ddf4f8694c3f74fa40370fe4b7f0c99748
jhosefer <JHOSEFER@itaipu.gov.br>
2022-05-13 17:07:20 -0300

    Refatoração para utilizar os novos parametros de Invoke-IBSNRestAPI

--- Public/func_Get-IBSNCatalog.ps1
+++ Public/func_Get-IBSNCatalog.ps1
@@ -6,29 +6,46 @@
         Obtem um Catálogo de Serviços.
         Caso não seja especificado, retorna todos os catálogos publicados na Instância.
     .PARAMETER ID
-        Especifica o SysID do Catálogo.
-    .OUTPUTS
-        Retorna um PSCustomObject do tipo IBSNCatalog.
+        Especifica a Identidade do catálogo a ser obtido.
+        O ID pode ser: SysID ou Name.
+    .PARAMETER Query
+        Critério de pesquisa na chamada Rest. A Sintaxe da Query pode ser consultada em https://docs.servicenow.com/bundle/rome-application-development/page/build/applications/concept/api-rest.html.
+        Obs: Uma forma fácil de obter a query é realizar os filtros diretamente no ServiceNow e utilizar o recurso "copy Query".
+    .PARAMETER ResultSize
+        Por padrão, apenas um número fixo de elementos são retornados em cada chamada Rest. 
+        Utilize o parâmetro ResultSize para especificar o número de itens que deseja. Para retornar todos os items, use: "-ResultSize Unlimited". Tenha em mente que dependendo do número de items, retornar todos os objetos pode levar bastante tempo e consumir bastante memória.
+    .EXAMPLE
+        Get-IBSNUser -ID user@domain.com
     #>
-    [CmdletBinding(DefaultParameterSetName='SET0')]
-    [OutputType([System.Object])]
+
+    [CmdletBinding(DefaultParameterSetName='SET1')]
+    [OutputType([int])]
     param(
-        [Parameter(Mandatory=$true,ParameterSetName='SET1',ValueFromPipeline=$true)]
-        [string]$ID
+        [Parameter(Mandatory=$false,Position=0,ParameterSetName='SET1')]
+        [string]$ID,
+
+        [Parameter(Mandatory=$false,Position=1,ParameterSetName='SET2')]
+        [string]$Query,
+
+        [Parameter(Mandatory=$false,Position=2,ParameterSetName='SET2')]
+        [ValidateScript({
+            ($_.ContainsKey('attribute') -and $_.ContainsKey('order'))
+            },
+            ErrorMessage = "Parâmetro deve ser especificado com as seguintes chaves: attribute='value' e order='asc|desc'"
+        )]
+        [Hashtable]$Sort,
+
+        [Parameter(Mandatory=$false,Position=3,ParameterSetName='SET1')]
+        [Parameter(Mandatory=$false,Position=3,ParameterSetName='SET2')]
+        [System.Object]$ResultSize
     )
 
-    $RestEndpoint = "api/sn_sc/servicecatalog/catalogs"
-
-    if ($PSBoundParameters.ContainsKey('ID')){
-        $URI = "$($ModuleControlFlags.InstanceURI)/$RestEndPoint/$ID`?sysparm_limit=1"
-    }
-    else {
-        $URI = "$($ModuleControlFlags.InstanceURI)/$RestEndPoint`?sysparm_limit=10000"
-    }
-
-    try { 
-        $Json = $(Invoke-IBSNRestAPI -URI $URI -Method GET -ErrorAction Stop).Result
-        $Json | ForEach-Object{$_.psobject.TypeNames.Insert(0, "IBSNCatalog")}; $Json  # Define a saida como um objeto do tipo IBSNCatalog
+    $Endpoint = "/api/sn_sc/servicecatalog/catalogs"
+    $Filtro = ($PSBoundParameters.ContainsKey('ID')) ? "sys_id=$ID^ORname=$ID" : $Query
+  
+    try {
+        $Json = Invoke-IBSNRestAPI -Resource $Endpoint -Query $Filtro -Sort $Sort -ResultSize $ResultSize
+        $Json | ForEach-Object{$_.psobject.TypeNames.Insert(0, "IBSNCatalog")}; $Json
     }
     catch{
         Write-Error "$($_.Exception.Message)"
