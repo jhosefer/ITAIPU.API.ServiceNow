df26524e948bd9aa8388d93e8102a35ae5c74957
jhosefer <JHOSEFER@itaipu.gov.br>
2022-05-13 09:09:57 -0300

    Melhoria no código e adição e adição de positional parameters

--- Public/func_Get-IBSNUser.ps1
+++ Public/func_Get-IBSNUser.ps1
@@ -20,27 +20,22 @@
     [CmdletBinding(DefaultParameterSetName='SET1')]
     [OutputType([int])]
     param(
-        [Parameter(Mandatory=$true,ParameterSetName='SET1')]
+        [Parameter(Mandatory=$true,Position=0,ParameterSetName='SET1')]
         [string]$ID,
 
-        [Parameter(Mandatory=$true,ParameterSetName='SET2')]
+        [Parameter(Mandatory=$false,Position=1,ParameterSetName='SET2')]
         [string]$Query,
 
-        [Parameter(Mandatory=$false,ParameterSetName='SET1')]
-        [Parameter(Mandatory=$false,ParameterSetName='SET2')]
+        [Parameter(Mandatory=$false,Position=3,ParameterSetName='SET1')]
+        [Parameter(Mandatory=$false,Position=3,ParameterSetName='SET2')]
         [System.Object]$ResultSize
     )
 
     $Endpoint = "/api/now/table/sys_user"
-    $Filtro = $PSCmdlet.ParameterSetName -eq 'SET1' ? "sys_id=$ID^ORuser_name=$ID^ORname=$ID" : $Query
+    $Filtro = ($PSCmdlet.ParameterSetName -eq 'SET1') ? "sys_id=$ID^ORuser_name=$ID^ORname=$ID" : $Query
   
     try {
-        if($PSBoundParameters.ContainsKey('ResultSize')){
-            $Json = Invoke-IBSNRestAPI -Resource $Endpoint -Query $Filtro -ResultSize $ResultSize
-        }
-        else {
-            $Json = Invoke-IBSNRestAPI -Resource $Endpoint -Query $Filtro
-        }
+        $Json = Invoke-IBSNRestAPI -Resource $Endpoint -Query $Filtro -ResultSize $ResultSize
         $Json | ForEach-Object{$_.psobject.TypeNames.Insert(0, "IBSNUser")}; $Json  # Define a saida como um objeto do tipo IBSNUser
     }
     catch{
